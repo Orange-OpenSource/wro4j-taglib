00459bbca886e3708c96e4787f81f1f0fd7fe36e
Julien Wajsberg <felash@gmail.com>
2012-05-12 22:16:51 +0200

    add a serial id for serialization conformance

--- src/main/java/com/francetelecom/saasstore/wro4j/taglib/ConfigurationException.java
+++ src/main/java/com/francetelecom/saasstore/wro4j/taglib/ConfigurationException.java
@@ -16,6 +16,8 @@
 package com.francetelecom.saasstore.wro4j.taglib;
 
 public class ConfigurationException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
 	public ConfigurationException() {
 		super();
 	}
