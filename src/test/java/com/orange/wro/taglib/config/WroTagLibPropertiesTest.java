f10ce506f66b6277d3141a095a49a544d096af07
Angelo Tata <angelo.tata@gmail.com>
2013-01-17 22:55:58 +0100

    Changed temporary folder creation to avoid platform path issues (see http://rationalpi.wordpress.com/2007/01/26/javaiotmpdir-inconsitency/)

--- src/test/java/com/orange/wro/taglib/config/WroTagLibPropertiesTest.java
+++ src/test/java/com/orange/wro/taglib/config/WroTagLibPropertiesTest.java
@@ -15,9 +15,8 @@
 */
 package com.orange.wro.taglib.config;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -33,106 +32,100 @@ import static org.junit.Assert.assertTrue;
  * @author Angelo Tata
  */
 public class WroTagLibPropertiesTest {
-    private static final String PROPERTIES_DEFAULT_LOCATION;
-    private static final String PROPERTIES_CUSTOM_LOCATION;
+	private String PROPERTIES_DEFAULT_LOCATION;
+	private String PROPERTIES_CUSTOM_LOCATION;
 
-    static {
-   		StringBuilder tempFolder = new StringBuilder(System.getProperty("java.io.tmpdir"));
+	@Rule
+	public TemporaryFolder tempFolder = new TemporaryFolder();
 
-        PROPERTIES_DEFAULT_LOCATION = tempFolder.toString();
-   		PROPERTIES_CUSTOM_LOCATION = tempFolder.append("wro4jtest").toString();
-   	}
+	@Before
+	public void createFoldersAndFiles() throws IOException {
+		PROPERTIES_DEFAULT_LOCATION = tempFolder.newFolder().getAbsolutePath();
+		PROPERTIES_CUSTOM_LOCATION = tempFolder.newFolder("wro4jtest").getAbsolutePath();
 
-    @BeforeClass
-   	public static void setUp() throws IOException {
-        new File(PROPERTIES_CUSTOM_LOCATION).mkdirs();
+		createProperties(PROPERTIES_DEFAULT_LOCATION, new String[]
+			{ DEFAULT_BASE_URL, DEFAULT_LESS_PATH, DEFAULT_RESOURCE_DOMAIN }
+		);
 
-        createProperties(PROPERTIES_DEFAULT_LOCATION, new String[]
-            { DEFAULT_BASE_URL, DEFAULT_LESS_PATH, DEFAULT_RESOURCE_DOMAIN }
-        );
-
-        createProperties(PROPERTIES_CUSTOM_LOCATION, new String[]
-            { CUSTOM_BASE_URL, CUSTOM_LESS_PATH, CUSTOM_RESOURCE_DOMAIN}
-        );
-   	}
-
-    @AfterClass
-   	public static void tearDown() {
-   		new File(PROPERTIES_DEFAULT_LOCATION).delete();
-   		new File(PROPERTIES_CUSTOM_LOCATION).delete();
-   	}
+		createProperties(PROPERTIES_CUSTOM_LOCATION, new String[]
+			{ CUSTOM_BASE_URL, CUSTOM_LESS_PATH, CUSTOM_RESOURCE_DOMAIN}
+		);
+	}
 
 
-    @Test
-    public void isNotAvailableWhenNoPropertiesAreFound() {
-        WroTagLibProperties wroTagLibProperties = new WroTagLibProperties(null, null, null);
+	private void createProperties(String location, String[] values) throws IOException {
+		File propertiesFile = new File(location, WroTagLibProperties.PROPERTIES_FILENAME_DEFAULT);
 
-        assertFalse("Should not be available, no properties anywhere", wroTagLibProperties.isAvailable());
-    }
+		Properties defaultProperties = new Properties();
+		defaultProperties.setProperty(WroTagLibConfig.InitParam.BASE_URL.getKey(), values[0]);
+		defaultProperties.setProperty(WroTagLibConfig.InitParam.LESS_PATH.getKey(), values[1]);
+		defaultProperties.setProperty(WroTagLibConfig.InitParam.RESOURCE_DOMAIN.getKey(), values[2]);
 
-    @Test
-    public void loadsPropertiesFromCustomLocationFirst() {
-        WroTagLibProperties wroTagLibProperties = new WroTagLibProperties(
-            PROPERTIES_DEFAULT_LOCATION, PROPERTIES_CUSTOM_LOCATION, null
-        );
+		FileWriter writer = new FileWriter(propertiesFile);
+		defaultProperties.store(writer, null);
+		writer.close();
+	}
 
-        assertTrue("Should be available", wroTagLibProperties.isAvailable());
-        assertEquals("Should return custom base url",
-                CUSTOM_BASE_URL, wroTagLibProperties.getProperty(WroTagLibConfig.InitParam.BASE_URL.getKey())
-        );
-        assertEquals("Should return custom Less path",
-            CUSTOM_LESS_PATH, wroTagLibProperties.getProperty(WroTagLibConfig.InitParam.LESS_PATH.getKey())
-        );
-        assertEquals("Should return custom resource domain",
-            CUSTOM_RESOURCE_DOMAIN, wroTagLibProperties.getProperty(WroTagLibConfig.InitParam.RESOURCE_DOMAIN.getKey())
-        );
-    }
 
-    @Test
-    public void loadsPropertiesFromDefaultLocationWhenCustomLocationUnavailable() {
-        WroTagLibProperties wroTagLibProperties = new WroTagLibProperties(
-            PROPERTIES_DEFAULT_LOCATION, null, null
-        );
+	@Test
+	public void isNotAvailableWhenNoPropertiesAreFound() {
+		WroTagLibProperties wroTagLibProperties = new WroTagLibProperties(null, null, null);
 
-        assertTrue("Should be available", wroTagLibProperties.isAvailable());
-        assertEquals("Should return default base url",
-            DEFAULT_BASE_URL, wroTagLibProperties.getProperty(WroTagLibConfig.InitParam.BASE_URL.getKey())
-        );
-        assertEquals("Should return default Less path",
-            DEFAULT_LESS_PATH, wroTagLibProperties.getProperty(WroTagLibConfig.InitParam.LESS_PATH.getKey())
-        );
-        assertEquals("Should return default resource domain",
-            DEFAULT_RESOURCE_DOMAIN, wroTagLibProperties.getProperty(WroTagLibConfig.InitParam.RESOURCE_DOMAIN.getKey())
-        );
-    }
+		assertFalse("Should not be available, no properties anywhere", wroTagLibProperties.isAvailable());
+	}
 
-    @Test
-    public void loadsPropertiesFromClasspathWhenNoPropertyFilesAvailable() {
-        WroTagLibProperties wroTagLibProperties = new WroTagLibProperties(
-            null, null, PROPERTIES_TEST_FILENAME
-        );
+	@Test
+	public void loadsPropertiesFromCustomLocationFirst() {
+		WroTagLibProperties wroTagLibProperties = new WroTagLibProperties(
+			PROPERTIES_DEFAULT_LOCATION, PROPERTIES_CUSTOM_LOCATION, null
+		);
 
-        assertTrue("Should be available", wroTagLibProperties.isAvailable());
+		assertTrue("Should be available", wroTagLibProperties.isAvailable());
+		assertEquals("Should return custom base url",
+				CUSTOM_BASE_URL, wroTagLibProperties.getProperty(WroTagLibConfig.InitParam.BASE_URL.getKey())
+		);
+		assertEquals("Should return custom Less path",
+			CUSTOM_LESS_PATH, wroTagLibProperties.getProperty(WroTagLibConfig.InitParam.LESS_PATH.getKey())
+		);
+		assertEquals("Should return custom resource domain",
+			CUSTOM_RESOURCE_DOMAIN, wroTagLibProperties.getProperty(WroTagLibConfig.InitParam.RESOURCE_DOMAIN.getKey())
+		);
+	}
 
-        assertEquals("Should return custom base url from classpath",
-            CLASSPATH_BASE_URL, wroTagLibProperties.getProperty(WroTagLibConfig.InitParam.BASE_URL.getKey())
-        );
-        assertEquals("Should return custom Less path from classpath",
-            CLASSPATH_LESS_PATH, wroTagLibProperties.getProperty(WroTagLibConfig.InitParam.LESS_PATH.getKey())
-        );
-        assertEquals("Should return custom resource domain from classpath",
-            CLASSPATH_RESOURCE_DOMAIN, wroTagLibProperties.getProperty(WroTagLibConfig.InitParam.RESOURCE_DOMAIN.getKey())
-        );
-    }
+	@Test
+	public void loadsPropertiesFromDefaultLocationWhenCustomLocationUnavailable() {
+		WroTagLibProperties wroTagLibProperties = new WroTagLibProperties(
+			PROPERTIES_DEFAULT_LOCATION, null, null
+		);
 
-    private static void createProperties(String location, String[] values) throws IOException {
-        File defaultPropertiesFile = new File(location, WroTagLibProperties.PROPERTIES_FILENAME_DEFAULT);
+		assertTrue("Should be available", wroTagLibProperties.isAvailable());
+		assertEquals("Should return default base url",
+			DEFAULT_BASE_URL, wroTagLibProperties.getProperty(WroTagLibConfig.InitParam.BASE_URL.getKey())
+		);
+		assertEquals("Should return default Less path",
+			DEFAULT_LESS_PATH, wroTagLibProperties.getProperty(WroTagLibConfig.InitParam.LESS_PATH.getKey())
+		);
+		assertEquals("Should return default resource domain",
+			DEFAULT_RESOURCE_DOMAIN, wroTagLibProperties.getProperty(WroTagLibConfig.InitParam.RESOURCE_DOMAIN.getKey())
+		);
+	}
 
-        Properties defaultProperties = new Properties();
-        defaultProperties.setProperty(WroTagLibConfig.InitParam.BASE_URL.getKey(), values[0]);
-        defaultProperties.setProperty(WroTagLibConfig.InitParam.LESS_PATH.getKey(), values[1]);
-        defaultProperties.setProperty(WroTagLibConfig.InitParam.RESOURCE_DOMAIN.getKey(), values[2]);
+	@Test
+	public void loadsPropertiesFromClasspathWhenNoPropertyFilesAvailable() {
+		WroTagLibProperties wroTagLibProperties = new WroTagLibProperties(
+			null, null, PROPERTIES_TEST_FILENAME
+		);
 
-        defaultProperties.store(new FileWriter(defaultPropertiesFile), null);
-    }
+		assertTrue("Should be available", wroTagLibProperties.isAvailable());
+
+		assertEquals("Should return custom base url from classpath",
+			CLASSPATH_BASE_URL, wroTagLibProperties.getProperty(WroTagLibConfig.InitParam.BASE_URL.getKey())
+		);
+		assertEquals("Should return custom Less path from classpath",
+			CLASSPATH_LESS_PATH, wroTagLibProperties.getProperty(WroTagLibConfig.InitParam.LESS_PATH.getKey())
+		);
+		assertEquals("Should return custom resource domain from classpath",
+			CLASSPATH_RESOURCE_DOMAIN, wroTagLibProperties.getProperty(WroTagLibConfig.InitParam.RESOURCE_DOMAIN.getKey())
+		);
+	}
 }
